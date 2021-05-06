7e465bb6d4846bc99462b0feb85dbc89b5e3b814
Alex <ignatenko2207@gmail.com>
2021-05-06 21:47:00 +0300

    Add comment

--- src/main/java/com/mainacademy/newsportal/api/rest/NewsContentController.java
+++ src/main/java/com/mainacademy/newsportal/api/rest/NewsContentController.java
@@ -3,10 +3,10 @@ package com.mainacademy.newsportal.api.rest;
 public class NewsContentController {
 
     // todo:
-    // agregated news (3 elements for each category grouped by category and sorted by time)
+    // agregated news (3 elements DTO for each category grouped by category and sorted by time)
 
-    // pageable news by category sorted by time (20 elements for each page)
+    // pageable news by category sorted by time (20 elements DTO for each page)
 
-    // news details by id
+    // news details by id (NOT DTO)
 
 }
